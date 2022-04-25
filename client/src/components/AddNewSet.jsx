54ec87a01529216ee9073465d636bb63abd34c13
Leonard94 <vladisla.koleda@yandex.ru>
2022-04-25 22:56:17 +0300

    Теперь предлагается добавить набор, если их нет
    - Компонент AddNewSet показывается, если у юзера нет ни одного набора
    - В этом компоненте к первой кнопке (отмена) добавлен тип button, иначе
      автоматически ставится тип submit и при нажатии на ентер срабатывает
      первая кнопка, а не кнопка подтверждения создания нового набора

--- client/src/components/AddNewSet.jsx
+++ client/src/components/AddNewSet.jsx
@@ -5,52 +5,65 @@ import { getSetsList, addNewSet } from '../store/sets/sets-actions'
 import addIcon from '../assets/icon/icon-add.svg'
 
 export function AddNewSet() {
-    const dispatch = useDispatch()
-    const [title, setTitle] = useState('')
-    const [addMode, setAddMode] = useState(false)
+	const dispatch = useDispatch()
+	const [title, setTitle] = useState('')
+	const [addMode, setAddMode] = useState(false)
 
-    const submit = (e) => {
-        e.preventDefault()
-        dispatch(addNewSet(title)).then(() => {
-            setTitle('')
-            dispatch(getSetsList())
-            toggleAddMode(false)
-        })
-    }
+	const submit = (e) => {
+		e.preventDefault()
+		dispatch(addNewSet(title)).then(() => {
+			setTitle('')
+			dispatch(getSetsList())
+			toggleAddMode(false)
+		})
+	}
 
-    const toggleAddMode = () => {
-        setAddMode(!addMode)
-    }
-    return (
-        <div className='add-set'>
-            <div className='add-set__body'>
-                {addMode && (
-                    <form className='add-set__form' onSubmit={submit}>
-                        <input
-                            type='text'
-                            placeholder='Введите имя набора'
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            maxLength='35'
-                            minLength='2'
-                        />
-                        <div className='add-set__btn-row'>
-                            <button className='add-set__btn add-set__btn--remove' onClick={toggleAddMode}>
-                                Отмена
-                            </button>
-                            <button className='add-set__btn add-set__btn--submit' type='submit'>
-                                Добавить
-                            </button>
-                        </div>
-                    </form>
-                )}
-                {!addMode && (
-                    <div className='add-set__add-body' onClick={toggleAddMode}>
-                        <img className='add-set__img' src={addIcon} alt='add new set' />
-                        <h6 className='add-set__subtitle'>Добавить новый набор</h6>
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+	const toggleAddMode = () => {
+		console.log('toggleAddMode')
+		setAddMode(!addMode)
+	}
+
+	return (
+		<div className='add-set'>
+			<div className='add-set__body'>
+				{addMode && (
+					<form className='add-set__form' onSubmit={submit}>
+						<input
+							type='text'
+							placeholder='Введите имя набора'
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
+							maxLength='35'
+							minLength='2'
+						/>
+						<div className='add-set__btn-row'>
+							<button
+								type='button'
+								className='add-set__btn add-set__btn--remove'
+								onClick={toggleAddMode}
+							>
+								Отмена
+							</button>
+							<button
+								className='add-set__btn add-set__btn--submit'
+								type='submit'
+							>
+								Добавить
+							</button>
+						</div>
+					</form>
+				)}
+				{!addMode && (
+					<div className='add-set__add-body' onClick={toggleAddMode}>
+						<img
+							className='add-set__img'
+							src={addIcon}
+							alt='add new set'
+						/>
+						<h6 className='add-set__subtitle'>Добавить новый набор</h6>
+					</div>
+				)}
+			</div>
+		</div>
+	)
 }
--- client/src/pages/SetsPage.jsx
+++ client/src/pages/SetsPage.jsx
@@ -9,30 +9,31 @@ import { getSetsList } from '../store/sets/sets-actions'
 import { selectSets } from '../store/sets/sets-selectors'
 
 export function SetsPage() {
-    const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-    const sets = useSelector(selectSets)
+	const sets = useSelector(selectSets)
 
+	useEffect(() => {
+		dispatch(getSetsList())
+	}, [dispatch])
 
-    useEffect(() => {
-        dispatch(getSetsList())
-    }, [dispatch])
-
-    return (
-        <div className='container'>
-            {/* <h2>Мои наборы</h2> */}
-            {/* {addMode && <AddNewSet back={setAddMode} />} */}
-            {/* {!addMode && <button onClick={() => setAddMode(true)}>Добавить новый набор</button>} */}
-            {!sets.length ? (
-                <h2>Вы еще не добавили ни одного набора</h2>
-            ) : (
-                <ul className='sets-card__list'>
-                    {sets.map((set) => (
-                        <SetsCard key={set._id} {...set} />
-                    ))}
-                    <AddNewSet />
-                </ul>
-            )}
-        </div>
-    )
+	return (
+		<div className='container'>
+			<ul className='sets-card__list'>
+				{!sets.length ? (
+					<>
+						<h2>Вы еще не добавили ни одного набора</h2>
+						<AddNewSet />
+					</>
+				) : (
+					<>
+						{sets.map((set) => (
+							<SetsCard key={set._id} {...set} />
+						))}
+						<AddNewSet />
+					</>
+				)}
+			</ul>
+		</div>
+	)
 }
